da2ec7b57e0fe136c76117a447f63f80853f5b7b
Balint <korka.balint@gmail.com>
2025-03-17 16:05:16 +0100

    inital development brach commit

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyDVIFrJphdsgiqLb4IHxV65f2IXAxz12L0",
+    authDomain: "swappy-hub-webapp.firebaseapp.com",
+    projectId: "swappy-hub-webapp",
+    storageBucket: "swappy-hub-webapp.firebasestorage.app",
+    messagingSenderId: "85179159865",
+    appId: "1:85179159865:web:2b4db966592a78cf3404e8",
+    measurementId: "G-NL0C4Z22L5"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -23,3 +23,10 @@ const auth = getAuth(app);
 export { db, storage, auth };
 
 
+// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
