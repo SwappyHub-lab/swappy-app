05fab9ca3b806d39b01056fcab92c58b535656bb
Balint <korka.balint@gmail.com>
2025-03-31 12:10:21 +0200

    added google analitics

--- src/pages/AddSwappy.js
+++ src/pages/AddSwappy.js
@@ -352,7 +352,7 @@ const sendMessage = async () => {
         <input type="text" name="title" placeholder="Title" value={formData.title} onChange={handleChange} />
         <input type="text" name="author" placeholder="Author" value={formData.author} onChange={handleChange} />
         <input className="input-description" type="text" name="description" placeholder="Description" value={formData.description} onChange={handleChange} />
-        <input type="text" name="socialmedia" placeholder="Social Media" value={formData.socialmedia} onChange={handleChange} />
+        <input type="text" name="socialmedia" placeholder="@Instagram" value={formData.socialmedia} onChange={handleChange} />
         {/* <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} /> */}
         <input type="file" accept="image/*" onChange={handleImageChange} />
         <div className="info-container">
